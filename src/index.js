07472c6f2dbe37a44b17db19848f6aa7c1cb4c35
whwjddn7364 <whwjddn7364@naver.com>
2021-01-03 01:28:02 +0900

    01.03

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const span = document.getElementById("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const reducer = (state = 0, action) => { //상태 데이터와 액션을 매개변수로 받아 새로운 상태를 반환하는 함수
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    case MINUS:
+      return state - 1;
+    default:
+      return state;
+  };
+};
+
+const countStore = createStore(reducer); //상태 데이터를 담는 공간
+
+const onChange = () => {
+  span.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange); // store 내부의 변화를 알려준다.
+
+const plusHandle = () => {
+  countStore.dispatch({type: ADD});
+  console.log(countStore.getState());
+};
+
+const minusHandle = () => {
+  countStore.dispatch({type: MINUS}); // dispatch를 이용하여 action을 reducer에게 전달해준다. 
+  console.log(countStore.getState()); // action은 상태를 어떻게 바꿀 것인지 알리고 그에 필요한 데이터 전달해준다.
+};
+
+plus.addEventListener("click", plusHandle);
+minus.addEventListener("click", minusHandle);
+
+console.log(countStore.getState());
